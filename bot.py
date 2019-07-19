1e0fc4b3c0206b5a0009c71bc6ba8ae02c186e2b
Abhishek Gupta <abhishek2411@outlook.com>
2019-07-19 06:39:34 +0100

    Add basic scaffolding for classes

---
+++ bot.py
@@ -0,0 +1,14 @@
+class Orientations():
+    pass
+
+class Movement():
+    pass
+
+class Commands():
+    pass
+
+class Planet():
+    pass
+
+class Robot():
+    pass
---
+++ test_bot.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class BotTest(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
