ac0ed2648794a5eee4eaaf0f604dc7e3f13a9319
Abhishek Gupta <abhishek2411@outlook.com>
2019-07-19 06:54:34 +0100

    Add basic structure to classes

--- bot.py
+++ bot.py
@@ -1,14 +1,43 @@
-class Orientations():
-    pass
+import numpy as np
+
+from enum import Enum
+
+class Orientations(Enum):
+    """Available Orientations"""
+    X_PLUS_ONE = "E"
+    X_MINUS_ONE = "W"
+    Y_PLUS_ONE = "N"
+    Y_MINUS_ONE = "S"
+
 
 class Movement():
     pass
 
-class Commands():
+class Commands(Enum):
+    """Available Commands"""
+    LEFT = "L"
+    RIGHT = "R"
+    FORWARD = "F"
     pass
 
 class Planet():
-    pass
+    """Planet is a rectangular grid
+    like [1, 1
+          1, 0]
+    where 1 represents general grid location
+    and 0 represents scented grid location
+    """
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.__grid = np.ones((x, y))
+    
 
 class Robot():
+    """Robot class"""
+    pass
+
+
+if __name__ == '__main__':
     pass
